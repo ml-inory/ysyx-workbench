026d0cba2a894f278967695e6064336d905eeb58
祈Inory <584688538@qq.com>
2024-01-28 18:34:31 +0800

    add bcdseg for Encoder83

---
+++ nju/Encoder83/csrc/main.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 祈Inory on 2024/1/28.
+// Copyright (c) 2024 BBK. All rights reserved.
+//
+/* verilator lint_off CASEX */
+#include <nvboard.h>
+#include <Vtop.h>
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+    dut.eval();
+}
+
+//static void reset(int n) {
+//    dut.rst = 1;
+//    while (n -- > 0) single_cycle();
+//    dut.rst = 0;
+//}
+
+int main() {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+//    reset(10);
+
+    while(1) {
+        nvboard_update();
+        single_cycle();
+    }
+}
---
+++ nju/Encoder83/vsrc/bcd8seg.v
@@ -0,0 +1,20 @@
+module bcd8seg(
+  input  [3:0] b,
+  output reg [7:0] h
+);
+    always @(*) begin
+        case(b)
+            4'h0:   h = ~8'b11111100;
+            4'h1:   h = ~8'b01100000;
+            4'h2:   h = ~8'b11011010;
+            4'h3:   h = ~8'b11110010;
+            4'h4:   h = ~8'b01100110;
+            4'h5:   h = ~8'b10110110;
+            4'h6:   h = ~8'b10111110;
+            4'h7:   h = ~8'b11100000;
+            4'h8:   h = ~8'b11111110;
+            4'h9:   h = ~8'b11110110;
+            default:    h = 8'hff;
+        endcase
+    end
+endmodule
